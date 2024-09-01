ad0b66a5099185500de91e950299a2f49c02bc7c
Abhijit-garedi <paljustwin@gmail.com>
2024-09-01 23:24:24 +0530

    (-,+,0 using nested if

---
+++ loops/nested_if.py
@@ -0,0 +1,14 @@
+#program 11
+
+n = int(input("Enter number: "))
+if n > 0:
+    if n >= 0:
+        print("positive")
+
+else:
+    if n == 0:
+        print("zero")
+    else:
+        print("negative")   
+   
+
