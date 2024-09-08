98b066ac333461c7f5477c225723cd9be3265839
Abhijit-garedi <paljustwin@gmail.com>
2024-09-09 00:13:10 +0530

    palindrome or not

--- Basics /d.py
+++ Basics /d.py
@@ -1,1 +1,10 @@
-jj
+def is_palindrome(num):
+    num_str = str(num)
+    return num_str == num_str[::-1]
+
+number = int(input("Enter a number: "))
+
+if is_palindrome(number):
+    print(f"{number} is a palindrome.")
+else:
+    print(f"{number} is not a palindrome.")
