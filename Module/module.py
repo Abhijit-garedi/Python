8fb8f3812a65633461888b3c00e9d2d254aa7182
Abhijit-garedi <paljustwin@gmail.com>
2024-10-01 18:46:08 +0530

    .

---
+++ Module/module.py
@@ -0,0 +1,17 @@
+#calculator .py
+
+def add(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x+y
+
+def divide(x, y):
+    if y != 0:
+        return x/y
+    else:
+        return "cannot divide by zero"        
+
