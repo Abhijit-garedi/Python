08863edc8d122f67f741428f7c31e03f4c393058
Abhijit-garedi <paljustwin@gmail.com>
2024-10-08 18:39:04 +0530

    pascal triangle

---
+++ pascal_triangle.py
@@ -0,0 +1,9 @@
+# program to print a pascal triangle
+
+def pascal_triangle(n):
+    tr = [1]
+    y = [0]
+    for i in range(n):
+        print (tr)
+        tr = [l+r for l, r in zip(tr+y, y+tr)]
+pascal_triangle(20)        
