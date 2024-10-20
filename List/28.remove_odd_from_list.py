4c98c2e071be33613ba8e6517f63135953a2bf14
Abhijit-garedi <paljustwin@gmail.com>
2024-10-20 23:49:28 +0530

    28

--- List/28.remove_odd_from_list.py
+++ List/28.remove_odd_from_list.py
@@ -1,19 +1,17 @@
 # python program to print a almmutative sum of the list of element
-
-# Function to calculate cumulative sum
 def cumulative_sum(lst):
-    cum_sum = []
-    total = 0
+    cum_sum = [] 
+    total = 0 
     for num in lst:
-        total += num  # Add current number to the total
-        cum_sum.append(total)  # Append the total to the cumulative sum list
-    return cum_sum
-
+        total += num 
+        cum_sum.append(total)  
+    return cum_sum  
 # Example usage
 n = int(input("Enter number of elements: "))  # Get the number of elements
 elements = []
 
-for i in range(n):  # Populate the list with user inputs
+for i in range(n):  
     elements.append(int(input(f"Enter element {i + 1}: ")))
 
-print("Cumulative Sum:", cumulative_sum(elements))  # Print the cumulative sum
+result = cumulative_sum(elements)  
+print("Cumulative sum list:", result)  
