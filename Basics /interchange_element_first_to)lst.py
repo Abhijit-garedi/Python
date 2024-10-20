58ea1d63da01da44165a8eb4792ea46cf2a3e399
Abhijit-garedi <paljustwin@gmail.com>
2024-10-20 21:26:44 +0530

    interchage the element of list

--- Basics /interchange_element_first_to)lst.py
+++ Basics /interchange_element_first_to)lst.py
@@ -1,13 +1,20 @@
 # write a python program to interchange the element of list first & last position 
 
-n = int(input())  
-for i in range(n):
-    l.append(i)  
-print(l) 
+# Input: Number of elements in the list
+n = int(input("Enter size of list: "))  
+l = []
 
+# Populating the list with values
+for i in range(n):
+    value = int(input("Enter value: "))
+    l.append(value)
+
+print("Original List:", l)
+
+# Interchanging the first and last element if the list has more than 1 element
 if len(l) > 1:  
     temp = l[0]
     l[0] = l[-1]
     l[-1] = temp
 
-print(l) 
+print("List after interchanging first and last elements:", l)
