396fc97234ff2abd9524ec27088372a19a685b80
Abhijit-garedi <paljustwin@gmail.com>
2024-10-20 21:25:26 +0530

    .

--- List/tempCodeRunnerFile.py
+++ List/tempCodeRunnerFile.py
@@ -5,9 +5,8 @@ l = []
 for i in range(n):
     i = int(input("enter value:"))
     l.append(i)
-
     print(l)
     index1 = int(input(i))
     index2 = int(input(j))
     l[index1],l[index2] = l[index2],l[index1]
-    print(l)# a python program to create a list and suffle the list of items using index values
+    print(l)
