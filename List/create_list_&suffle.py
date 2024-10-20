8b4167e37f7bece56707a9c20eb709c2ca3cc87d
Abhijit-garedi <paljustwin@gmail.com>
2024-10-20 21:28:24 +0530

    create list and suffle elements

--- List/create_list_&suffle.py
+++ List/create_list_&suffle.py
@@ -1,13 +1,20 @@
 # a python program to create a list and suffle the list of items using index values
 
-n = int (input("enter size:"));
+n = int(input("Enter size: "))
 l = []
-for i in range(n):
-    i = int(input("enter value:"))
-    l.append(i)
 
-    print(l)
-    index1 = int(input(i))
-    index2 = int(input(j))
-    l[index1],l[index2] = l[index2],l[index1]
-    print(l)# a python program to create a list and suffle the list of items using index values
+# Input list values
+for i in range(n):
+    value = int(input("Enter value: "))
+    l.append(value)
+
+print("Original list:", l)
+
+# Input indexes to shuffle
+index1 = int(input("Enter the first index to swap: "))
+index2 = int(input("Enter the second index to swap: "))
+
+# Swap the elements at index1 and index2
+l[index1], l[index2] = l[index2], l[index1]
+
+print("List after swapping:", l)
