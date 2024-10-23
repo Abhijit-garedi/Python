e44bb9c157e3496d89aa1154a0f960e28ad00790
Abhijit-garedi <paljustwin@gmail.com>
2024-10-23 21:16:32 +0530

    original state

--- 43_1to20and1_3_values.py
+++
@@ -1,6 +0,0 @@
-s=dict()
-n=int(input("enter n value:"))
-for i in range(1,n+1):
-    s[i]=1**2
-    print(s)
-    
--- 44_check_given_string_palindrome.py
+++
@@ -1,6 +0,0 @@
-s=input("enter string")
-s1=s[:: 1]
-if (s==1):
-    print("palindrome")
-else:
-    print("not palindrome")    
