370f524662e9176f27237dfeedf246683b25d58b
Abhijit-garedi <paljustwin@gmail.com>
2024-10-26 18:24:01 +0530

    program no 72

---
+++ unit 5/72_class,disp.py
@@ -0,0 +1,9 @@
+
+class A:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        return "my name is {} and {} old".format(self.name, self.age)
+
