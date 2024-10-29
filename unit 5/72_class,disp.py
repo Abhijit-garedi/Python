5ebfd7938582d48f9f50570cf9940fca043aec62
Abhijit-garedi <paljustwin@gmail.com>
2024-10-29 12:55:41 +0530

    program 72

--- unit 5/72_class,disp.py
+++ unit 5/72_class,disp.py
@@ -5,5 +5,7 @@ class A:
         self.age = age
 
     def display(self):
-        return "my name is {} and {} old".format(self.name, self.age)
+        return "my name is {} and i am {} years old".format(self.name, self.age)
 
+person = A("Avijit", 20)
+print(person.display())
