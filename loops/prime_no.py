06b61c2b32d04a49012e04b80a6b158009c18a41
Abhijit-garedi <paljustwin@gmail.com>
2024-11-06 23:10:28 +0530

    prime numbers between two numbers corrected program

--- loops/prime_no.py
+++ loops/prime_no.py
@@ -7,9 +7,7 @@ b = int(input("second num: "))
 for i in range(a, b + 1):
     if i > 1:
         for j in range(2, i):
-             if(i % j == 0):
+            if i % j == 0:
                 break
-
-    else:
-                print(i)
-
+        else:
+            print(i)
