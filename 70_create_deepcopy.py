c2974c7bb8a54c438ff1d8158e54636642ecf23e
Abhijit-garedi <paljustwin@gmail.com>
2024-11-11 21:20:48 +0530

    program 70 create deep copy

---
+++ 70_create_deepcopy.py
@@ -0,0 +1,8 @@
+#wapp to create a deep copy of the given sequence
+import copy
+nums_x[1,[2,3,4]]
+print("original list:num_x")
+nums_y=copy.deepcopy(nums_x)
+print("\ndeep copy of the said list:")
+print(nums_y)
+print("\nchange the value of ")
