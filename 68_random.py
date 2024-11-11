0a4119e5cb45613be1da8cc38a92d81e69e62d9c
Abhijit-garedi <paljustwin@gmail.com>
2024-11-11 21:19:53 +0530

    progrm 68 random

---
+++ 68_random.py
@@ -0,0 +1,13 @@
+
+import random
+import string
+
+print("Generate a random alphabet")
+print(random.choice(string.ascii_letters))
+
+Max_length=255
+str1 = ""
+
+for i in range(random.randint(1, Max_length)):
+    str1 += random.choice(string.ascii_letters)
+print(str1)    
