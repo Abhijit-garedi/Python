2e650d0cc7a3c5eaf1bb96ccffbad522fe3a4b7a
Abhijit-garedi <paljustwin@gmail.com>
2024-11-16 21:51:50 +0530

    stack program

---
+++ Internal Prep/list/Stack_Queues.py
@@ -0,0 +1,15 @@
+# Stack & Queues
+
+stack = []
+stack.append('a')
+stack.append('b')
+stack.append('c')
+print("initial stack")
+print(stack)
+print('\n Elements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print('\n stack after elements are popped')
+print(stack)
+ 
