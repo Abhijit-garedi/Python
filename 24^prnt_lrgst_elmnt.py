6025539126d815e1ecb54fd4cccde24e361a306e
Abhijit-garedi <paljustwin@gmail.com>
2024-11-18 00:54:31 +0530

    create list using append extend and insert

---
+++ 24^prnt_lrgst_elmnt.py
@@ -0,0 +1,13 @@
+# Write a python program to create a list using append(),extend(),insert()
+
+k=[1,2.1,'krishna']
+k.append(4)
+print(k)
+k.extend([2,'chaitanya'])
+print(k)
+k.insert(3,'444')
+print(k)
+
+# [1, 2.1, 'krishna', 4]
+# [1, 2.1, 'krishna', 4, 2, 'chaitanya']
+# [1, 2.1, 'krishna', '444', 4, 2, 'chaitanya']
