04245a788103763cbdd13b6e02ed665ded58ecea
Abhijit-garedi <paljustwin@gmail.com>
2024-11-19 00:46:31 +0530

    Create User defined function & print student Name&age

---
+++ 51_defined_function_prnt_stdnt_nm_ag.py
@@ -0,0 +1,8 @@
+# Pr 51:-
+# WAPP to create a used defined function and print student name and age
+
+def fn_write(name,age):
+    print("welcome{},{}".format(name,age))
+n=input("enter name")
+a=int(input("enter age:"))
+fn_write(n ,a)
