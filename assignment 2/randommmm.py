154259fc1905c9eb904167165836fff8f117b9fc
Abhijit-garedi <paljustwin@gmail.com>
2024-11-21 19:57:46 +0530

    module built in functions

---
+++ assignment 2/randommmm.py
@@ -0,0 +1,30 @@
+#Modules bult in function 
+
+# Random
+import random  
+n = random.randint(1, 5)
+print(n) 
+
+import random
+n = random.randrange(1, 5)
+print(n)
+
+
+#statistics
+import statistics
+d = statistics.mean([1, 2, 3, 5,6])
+print(d)
+
+import statistics
+a = statistics.median([1,2,3,4,6])
+print(a)
+
+
+#math
+import math
+n = math.sqrt(4)
+print(n)
+
+import math
+a = math.factorial(4)
+print(a)
