cbe96e4724f1d851d2f3d1f217aa2ef3430348e5
Abhijit-garedi <paljustwin@gmail.com>
2024-12-07 20:03:19 +0530

    verification

---
+++ assignment 2/recognised_or_not.py
@@ -0,0 +1,14 @@
+tn = input("Enter name: ")
+
+
+if tn == "oli":  # Changed `n` to `tn` to use the input value
+    print("Prime Minister")
+
+elif tn == "balen":
+    print("mayor")
+       
+else:
+    print("Not Recognised")
+
+
+
