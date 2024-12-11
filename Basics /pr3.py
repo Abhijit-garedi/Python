66e1a7128bdbcd3a3daf2ce75fa885f870586262
Abhijit-garedi <paljustwin@gmail.com>
2024-12-11 21:15:25 +0530

    comparison operators

---
+++ Basics /pr3.py
@@ -0,0 +1,11 @@
+ #Write a python program to evaluate all the comparison operators
+x = int(input("Enter x value: "))
+y = int(input("Enter y value: "))
+
+print("x > y:", x > y)
+print("x < y:", x < y)
+print("x <= y:", x <= y)
+print("x >= y:", x >= y)
+print("x == y:", x == y)
+print("x != y:", x != y)
+
