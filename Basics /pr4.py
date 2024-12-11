66de555cb8a023a6ce7c9e0ce9e14029d7ece76d
Abhijit-garedi <paljustwin@gmail.com>
2024-12-11 21:19:53 +0530

    pr 4

---
+++ Basics /pr4.py
@@ -0,0 +1,8 @@
+#Write a python program to evaluate all the logical operators
+x = int(input("Enter x value: "))
+y = int(input("Enter y value: "))
+
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
+print("not y:", not y)
