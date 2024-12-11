52eb370161ce2b870a40ce6c7bcb8a98261cbc96
Abhijit-garedi <paljustwin@gmail.com>
2024-12-11 21:04:19 +0530

    program 2

---
+++ Basics /pr2.py
@@ -0,0 +1,15 @@
+# to evaluate all the arthmetic operators
+
+x = int(input("enter x value:"))
+
+y = int(input("enter y value:"))
+
+print("x+y",x+y)
+
+print("x-y",x-y)
+
+print("x*y",x*y)
+
+print("x/y",x/y)
+
+print("x%y",x%y)
