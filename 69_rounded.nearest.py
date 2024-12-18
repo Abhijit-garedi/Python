5898538512c9a81e2441a14cf7309b13dd38e450
Abhijit-garedi <paljustwin@gmail.com>
2024-12-18 22:12:24 +0530

    shifted

--- 69_rounded.nearest.py
+++
@@ -1,8 +0,0 @@
-#program for rounder nearest
-
-import decimal
-decimal.getcontext().prec=1
-decimal.getcontext().rounding=decimal.ROUND_HALF_EVEN
-print(decimal.Decimal(10)/decimal.Decimal(4))
-
-# output 2
