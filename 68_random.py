06e25ca1755a6dc72cc5b5f0db4fdfd9a0b8598b
Abhijit-garedi <paljustwin@gmail.com>
2024-12-18 21:32:35 +0530

    program to generate random

--- 68_random.py
+++ 68_random.py
@@ -11,3 +11,8 @@ str1 = ""
 for i in range(random.randint(1, Max_length)):
     str1 += random.choice(string.ascii_letters)
 print(str1)    
+
+
+# output
+# l
+# dhoewohbosdhieghkdngoehgheokhwehogihkgbh
