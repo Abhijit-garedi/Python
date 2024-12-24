6ae3918cceec91459b649915b623e8ff4652774c
Abhijit-garedi <paljustwin@gmail.com>
2024-12-24 15:03:36 +0530

    create package

---
+++ pr65_create_pckg_pck1.py
@@ -0,0 +1,11 @@
+#WAPP to create a package name pack 1 import functions of mod1 have a pangram and pascal triangle
+-create a folder with name pack 1
+-save an empty file __init__.py
+-keep the file pr60.py in pack1
+-keep the file pr62.py in pack 1
+->create a new program file pr65.py
+
+from pack1.pr60 import*
+         is_pangram("    ")
+from pack1.pr62 import*
+         pascal_triangle(6)
