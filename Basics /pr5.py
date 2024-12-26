11d2c7e4fe8e452a11a27e70ebe42a0a3e51a6af
Abhijit-garedi <paljustwin@gmail.com>
2024-12-26 12:59:06 +0530

    dubli

--- Basics /pr5.py
+++
@@ -1,7 +0,0 @@
-# to evaluate all teh bitwise operators
-x = int(input("enter x value:"))
-y = int(input("enter y value:"))
-print("x&y", x & y)
-print("x/y",x/y)
-print("~x:",~x)
-print("x^y:",x^y)
