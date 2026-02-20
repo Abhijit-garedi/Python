a99aee388d7e77c9119c133c811d030a8a43673d
Abhijit-garedi <paljustwin@gmail.com>
2026-02-20 23:44:19 +0530

    changed location

---
+++ labs programs/2_evulat_arithmetic_operations.py
@@ -0,0 +1,28 @@
+#write a python program to evaluate all the arthmetic operations
+
+x=int(input("enter x value"))
+y=int(input("enter y value"))
+
+print("x+y is:",x+y)
+
+print("x-y is",x-y)
+
+print("x*y is:",x*y)
+
+print("x/y is:",x/y)
+
+print("x%y is:", x%y)
+
+print("x**y is ", x**y)
+
+print("x//y is", x//y)
+
+#output enter x value3
+#enter y value1
+#x+y is: 4
+#x-y is 2
+#x*y is: 3
+#x/y is: 3.0
+#x%y is: 0
+#x**y is  3
+#x//y is 3 
